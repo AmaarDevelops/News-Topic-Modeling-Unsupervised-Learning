167b45d357d50b52959ac450c096c00bfc1c8fd6
AmaarDevelops <amaarkhaskeli1@gmail.com>
2025-09-18 13:06:37 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         drawWordCloud([]);
 
         try {
-            const response = await fetch('http://localhost:5000/predict', {
+            const response = await fetch('/predict', {
                 method : 'POST',
                 headers : {
                     'Content-Type' : 'application/json'
