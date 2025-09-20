973beca747139a20810347432b42308c37028c54
AmaarDevelops <amaarkhaskeli1@gmail.com>
2025-09-20 20:10:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import joblib
